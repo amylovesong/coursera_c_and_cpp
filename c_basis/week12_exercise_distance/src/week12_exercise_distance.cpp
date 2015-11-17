e7266d4acf90547a5d25ca5f2f19a5f30b4f2fbe
sunxiaoling <amylovesong.sun@gmail.com>
2015-11-17 15:38:51 +0800

    update c basis week12 exercise: the furthest distance

--- c_basis/week12_exercise_distance/src/week12_exercise_distance.cpp
+++ c_basis/week12_exercise_distance/src/week12_exercise_distance.cpp
@@ -8,45 +8,40 @@
 #include <iostream>
 #include <iomanip>
 #include <stdio.h>
+#include <math.h>
 using namespace std;
-int main() {
-	int n = 0;
-//	float start = 0.0f, end = 0.0f, tmp = 0.0f;
-//	float p[2] = { 0.0f };
-	double start = 0.0, end = 0.0, tmp = 0.0;
-	double p[2] = { 0.0 };
-	int flag = 0;
-	cin >> n;
-	while (n > 0) {
-		scanf("%lf", &start);
-		scanf("%lf", &end);
-		if (start > end) {
-			tmp = start;
-			start = end;
-			end = tmp;
-		}
-		if (flag == 0) {
-			p[0] = start;
-			p[1] = end;
-			flag = 1;
-		} else {
-			if (start < p[0]) {
-				p[0] = start;
-			}
-			if (end > p[1]) {
-				p[1] = end;
-			}
-		}
-//		cout << "n=" << n
-////				<< fixed << setprecision(4)
-//				<< " p[0]=" << p[0]
-//				<< " p[1]=" << p[1] << endl;
 
-		n--;
+int main() {
+	float p_x = 0.0f, p_y = 0.0f;
+	double dis = 0.0, tmp = 0.0;
+	int flag = 0;
+	int n = 0;
+	cin >> n;
+	double x[n], y[n];
+
+	for (int i = 0; i < n; i++) {
+		scanf("%f", &p_x);
+		scanf("%f", &p_y);
+		x[i] = p_x;
+		y[i] = p_y;
 	}
-	p[1] = 67.1;
-	p[0] = 14.7;
-	cout << fixed << setw(4) << (p[1] - p[0]) << endl;
+
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			tmp = sqrt(
+					(x[i] - x[j]) * (x[i] - x[j])
+							+ (y[i] - y[j]) * (y[i] - y[j]));
+			if (flag) {
+				if (dis < tmp) {
+					dis = tmp;
+				}
+			} else { //Initialize
+				flag = 1;
+				dis = tmp;
+			}
+		}
+	}
+	cout << fixed << setprecision(4) << dis << endl;
 
 	return 0;
 }
