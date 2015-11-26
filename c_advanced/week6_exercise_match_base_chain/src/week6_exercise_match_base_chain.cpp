d59d411edb667c316afeca15a75fd1ef187ea865
sunxiaoling <amylovesong.sun@gmail.com>
2015-11-26 15:27:02 +0800

    week6 exercise: match base chain

---
+++ c_advanced/week6_exercise_match_base_chain/src/week6_exercise_match_base_chain.cpp
@@ -0,0 +1,45 @@
+/*
+ * week6_exercise_match_base_chain.cpp
+ *
+ *  Created on: 2015年11月26日
+ *      Author: sunxiaoling
+ */
+
+#include <iostream>
+using namespace std;
+
+void match_base_chain(char *p) {
+	while (*p != '\0') {
+		switch (*p) {
+		case 'A':
+			cout << 'T';
+			break;
+		case 'T':
+			cout << 'A';
+			break;
+		case 'G':
+			cout << 'C';
+			break;
+		case 'C':
+			cout << 'G';
+			break;
+		}
+		p++;
+	}
+	cout << endl;
+}
+
+int main() {
+	int n = 0;
+	char base_chain[256] = { '\0' };
+	cin >> n;
+
+	while (n > 0) {
+		cin >> base_chain;
+		match_base_chain(base_chain);
+		n--;
+	}
+
+	return 0;
+}
+
