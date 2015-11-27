f75b5a465748b17757b24606ce7369d5c2029454
sunxiaoling <amylovesong.sun@gmail.com>
2015-11-27 17:05:34 +0800

    final exam: num contains 3

---
+++ c_advanced/final_exam_num_contains_3/src/final_exam_num_contains_3.cpp
@@ -0,0 +1,53 @@
+/*
+ * final_exam_num_contains_3.cpp
+ *
+ *  Created on: 2015年11月27日
+ *      Author: sunxiaoling
+ */
+
+#include <iostream>
+using namespace std;
+
+const int MAX_NUM_LENGTH = 6;
+int index = 0;
+
+char * convertInt2Array(int num, char *s) {
+	if (num / 10 != 0) {
+		convertInt2Array(num / 10, s);
+	}
+	s[index++] = num % 10 + 48;
+	return s;
+}
+
+void check(int num, int k) {
+	bool flag = false;
+	char s_num[MAX_NUM_LENGTH + 1] = { '\0' };
+	convertInt2Array(num, s_num);
+	if (num % 19 == 0) {
+		int i = 0, count = 0;
+		while (s_num[i] != '\0') {
+			if (s_num[i] == '3') {
+				count++;
+			}
+			i++;
+		}
+		if (count == k) {
+			flag = true;
+		}
+	}
+
+	if (flag) {
+		cout << "YES" << endl;
+	} else {
+		cout << "NO" << endl;
+	}
+}
+
+int main() {
+	int m = 0, k = 0;
+	cin >> m >> k;
+	check(m, k);
+
+	return 0;
+}
+
